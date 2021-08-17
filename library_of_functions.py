3b4911c35742b0f15b643cc540325421b7d2de60
polinatrump <68547236+polinatrump@users.noreply.github.com>
2021-08-17 12:38:02 +0300

    Update library_of_functions.py

--- library_of_functions.py
+++ library_of_functions.py
@@ -1,15 +1,13 @@
-import serial
-import cv2
 import numpy as np
-import time
 
 class RobotControl():
     def __init__(self):
+        self.frame = []
+        self.IR = 'SI010010010010010010010010E'
         self.IR_last_msg = 'ST0+00000+00000E'
+        self.read_data_4_US = ['SUF030E\r\n', 'SUL030E\r\n', 'SUB030E\r\n', 'SUR030E\r\n']
         self.UV_last_error = 0
         self.data_4_US = []
-        self.frame_rate = 5
-        self.prev = 0
         self.ser = serial.Serial('/dev/ttyUSB0')
         self.ser.baudrate = 115200
         self.cap = cv2.VideoCapture(0)  # замена на VideoStream
@@ -26,15 +24,16 @@ class RobotControl():
         newcameramatrix, roi = cv2.getOptimalNewCameraMatrix(K, d, (w, h), 0)
         self.mapx, self.mapy = cv2.initUndistortRectifyMap(K, d, None, newcameramatrix, (w, h), 5)
 
-    def IR_reading(self):  # функция считывания данных с ИК датчика
+    def data_reading_IR(self):  # функция считывания данных с ИК датчика
         try:
             US_IR = self.ser.readline()
             US_IR = US_IR.decode()
             if US_IR[:2] == 'SI':
-                return US_IR
+                self.IR = US_IR
+                print('self IR', self.IR)
         except Exception as e:
-            US_IR = 'SI010010010010010010010010E'
-            return US_IR
+            pass
+        return self.IR
 
     def IR_wheel_movement(self, moving):
         wheel_movements_dict = {'LEFT': 'ST0+00250-00250E', 'RIGHT': 'ST0-00250+00250E', 'FORWARD': 'ST0+00300+00300E'}
@@ -56,12 +55,11 @@ class RobotControl():
             if US_IR[:2] == 'SU':
                 self.data_4_US.append(US_IR)
                 if len(self.data_4_US) == 4:
-                    readed_data_4_US = self.data_4_US
+                    self.read_data_4_US = self.data_4_US
                     self.data_4_US = []
-                    return readed_data_4_US
         except Exception as e:
-            readed_data_4_US = ['SUF030E\r\n', 'SUL030E\r\n', 'SUB030E\r\n', 'SUR030E\r\n']
-            return readed_data_4_US
+            pass
+        return self.read_data_4_US
 
     def US_wheel_movement(self, moving):
         wheel_movements_dict = {'LEFT': 'ST0+00250+00050E', 'RIGHT': 'ST0-00000+00250E', 'FORWARD': 'ST0+00300+00300E'}
@@ -76,13 +74,12 @@ class RobotControl():
         self.ser.write(msg_UV)
 
     def camera_reading(self):
-        time_elapsed = time.time() - self.prev  # для уменьшения частоты кадров
-        if time_elapsed > 1. / self.frame_rate:
-            self.prev = time.time()
-            ret, frame = self.cap.read()
-            frame = cv2.resize(frame, self.dim, interpolation=cv2.INTER_AREA)  # уменьшаем разрешение фото
-            frame = cv2.remap(frame, self.mapx, self.mapy, cv2.INTER_LINEAR)  # избавляемся от дисторсии
-            return frame
+        ret, frame = self.cap.read()
+        print(self.dim)
+        frame = cv2.resize(frame, self.dim, interpolation=cv2.INTER_AREA)  # уменьшаем разрешение фото
+        frame = cv2.remap(frame, self.mapx, self.mapy, cv2.INTER_LINEAR)  # избавляемся от дисторсии
+        return frame
+
     def line_area_selection(self, frame, max_cntr, mask):
         x, y, w, h = cv2.boundingRect(max_cntr)
         black = np.zeros((frame.shape[0], frame.shape[1], 3), np.uint8)  # ---black in RGB
@@ -90,20 +87,19 @@ class RobotControl():
         aim_area = cv2.bitwise_and(frame, max_cntr_area, mask=mask)
         return aim_area
 
-    def splitting_frame_into_parts(self, frame, imgheight, width, mask):
+    def splitting_frame_into_parts(self, frame, j, imgheight, width, mask):
         black = np.zeros((frame.shape[0], frame.shape[1], 3), np.uint8)
         grid = cv2.rectangle(black, (0, j), (0 + imgheight, j + width), (0, 255, 0), -1)
         aim_area_part = cv2.bitwise_and(frame, grid, mask=mask)
         return aim_area_part
 
     def UV_wheel_movement(self, moving, x_point):
-        wheel_movements_dict = {'SHARPLY_LEFT': 'ST0-00100+00250E', 'SHARPLY_RIGHT': 'ST0+00250-00100E',
+        wheel_movements_dict = {'SHARPLY_LEFT': 'ST0+00250-00100E', 'SHARPLY_RIGHT': 'ST0-00100+00250E',
                                 'FORWARD': 'ST0+00300+00300E',
                                 'SOFTLY_RIGHT': 'ST0+00200+00300E', 'RIGHT': 'ST0+00100+00300E',
                                 'SOFTLY_LEFT': 'ST0+00300+00200E',
                                 'LEFT': 'ST0+00300+00100E'}
         msg = wheel_movements_dict.get(moving)
         self.UV_last_error = x_point
-        print('msg --- ', msg)
         msg = msg.encode('utf-8')
         self.ser.write(msg)
